70992e5c4c565486223b7ebd62ee24dc12cb6d35
Aline Sanches <sanches.aline6@gmail.com>
2024-04-28 10:57:12 -0300

    Inclui método abstrado da interface Transacao

--- model/Deposito.py
+++ model/Deposito.py
@@ -1,5 +1,11 @@
+from .Conta import Conta
 from .Transacao import Transacao
 
 class Deposito(Transacao):
     def __init__(self, valor) -> None:
         self.valor = valor
+
+    def registrar(self, conta: Conta):
+        return super().registrar(conta)
+    
+    
--- model/Saque.py
+++ model/Saque.py
@@ -1,5 +1,9 @@
+from .Conta import Conta
 from Transacao import Transacao
 
 class Saque(Transacao):
     def __init__(self, valor) -> None:
         self.valor = valor
+
+    def registrar(self, conta: Conta):
+        return super().registrar(conta)
